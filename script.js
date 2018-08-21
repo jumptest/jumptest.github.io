71d591dccddc6b1ab0ad876d904fa180bda2cdbb
chenjie <chenjie@hujiang.com>
2018-08-21 18:14:42 +0800

    123
    12312

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function isWeixinBrowser(){
-  var ua = navigator.userAgent.toLowerCase();
-  return (/micromessenger/.test(ua)) ? true : false ;
+function isWeixinBrowser() {
+  var ua = navigator.userAgent.toLowerCase()
+  return /micromessenger/.test(ua) ? true : false
 }
 
+window.onload = function() {
+  var el = document.getElementById('tk')
 
-window.onload= function() {
-  var el = document.getElementById("tk")
-  if (isWeixinBrowser()) {
-    el.innerHTML = "isWx"
+  console.log('window loaded ')
+  console.log(navigator.userAgent.toLowerCase())
+  let go = isWeixinBrowser()
+  console.log(go)
+
+  if (go) {
+    el.innerHTML = 'isWx'
   } else {
     el.innerHTML = 'isNotWx'
   }
